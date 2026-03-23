2a9720117eec5b50ba43e67dc57c8e64853a2f01
josephbutcher-johns <rpkj0632@leeds.ac.uk>
2026-03-23 14:46:03 +0000

    saved

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -5,11 +5,46 @@
 #include "shapes.h"
 
 Point makePoint( float x, float y ) {
-    Point new;
+    Point p;
     // implementation
-    return new;
+    p.x = x;
+    p.y = y;
+    return p;
 }
 
+Line makeLine(Point p1,  Point p2 ){
+    Line l;
+    l.p[0] = p1;
+    l.p[1] = p2;
+    return l;
+}
+
+Triangle makeTriangle (Point p1,  Point p2 ,Point p3 ){
+    Triangle t;
+    t.p[0] = p1;
+    t.p[1] = p2;
+    t.p[2] = p3;
+    return t;
+}
+
+
+float lineLength( Line l ){ 
+    // 
+    float 
+}       
+float triangleArea( Triangle t ){
+
+}
+
+bool samePoint( Point p1, Point p2 ){
+
+}         
+bool pointInLine( Point p, Line l){
+
+}          
+bool pointInTriangle( Point p, Triangle t ){
+
+} 
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
