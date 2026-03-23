55428d3cae5b47488a44beaad274d62759f4fbb6
josephbutcher-johns <rpkj0632@leeds.ac.uk>
2026-03-23 16:40:04 +0000

    saved

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -1,4 +1,5 @@
-
+// Joseph Butcher-Johns
+//ID: 201948424
 #include <stdbool.h>
 #include <math.h>
 
@@ -29,22 +30,37 @@ Triangle makeTriangle (Point p1,  Point p2 ,Point p3 ){
 
 
 float lineLength( Line l ){ 
-    // 
-    float 
+    // distance formula 
+    float p1a = l.p[0].x - l.p[1].x;
+    float p2a = l.p[0].y - l.p[1].y;
+    float length = sqrt((p1a * p1a) + (p2a *p2a));
 }       
-float triangleArea( Triangle t ){
 
+
+float triangleArea( Triangle t ){
+    // area = 1/2(x1(y2-y3) + x2(y3-y1) + x3(y1-y2))
+    float x1 = t.p[0].x;
+    float y1 = t.p[0].y;
+    float x2 = t.p[1].x;
+    float y2 = t.p[1].y;
+    float x3 = t.p[2].x;
+    float y3 = t.p[2].y;
+    float area = 0.5 * ((x1 * (y2-y3)) + (x2 * (y3-y1)) + (x3 * (y1-y2)));
+    return area;
 }
 
-bool samePoint( Point p1, Point p2 ){
+bool samePoint(Point p1, Point p2) {
+    // fabsf should use the absolute value with floats
+    return (fabsf(p1.x - p2.x) < 1.0e-6) && (fabsf(p1.y - p2.y) < 1.0e-6);
+}
+// following functions i decided to use the samePoint to keep it simple as it is essentially checking the same thing 
+bool pointInLine(Point p, Line l) {
+    return samePoint(p, l.p[0]) || samePoint(p, l.p[1]);
+}
 
-}         
-bool pointInLine( Point p, Line l){
-
-}          
-bool pointInTriangle( Point p, Triangle t ){
-
-} 
+bool pointInTriangle(Point p, Triangle t) {
+    return samePoint(p, t.p[0]) || samePoint(p, t.p[1]) || samePoint(p, t.p[2]);
+}
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
